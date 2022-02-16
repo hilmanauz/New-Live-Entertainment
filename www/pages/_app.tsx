cbe58a3eb74e8c68a29acf04a0f63e9d88b09e87
hilmanauz <hilmanauz@gmail.com>
2022-02-16 16:25:35 +0700

    delete theme.tsx

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,8 +1,73 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import Head from 'next/head'
-import theme from './theme'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        fontFamily: "'Roboto', sans-serif",
+        color: "#171717",
+        fontSize: "14px",
+        fontWeight: "400",
+      },
+    },
+  },
+  components: {
+    FormLabel: {
+      baseStyle: {
+        fontSize: "14px",
+      },
+    },
+    Select: {
+      variants: {
+        outline: {
+          field: {
+            bg: "white",
+          },
+        },
+      },
+      defaultProps: {
+        size: "sm",
+      },
+    },
+    Switch: {
+      defaultProps: {
+        size: "md",
+      },
+    },
+    Input: {
+      variants: {
+        outline: {
+          field: {
+            bg: "white",
+          },
+        },
+      },
+      sizes: {
+        sm: {
+          borderRadius: "4px",
+          px: "13px",
+        },
+      },
+      defaultProps: {
+        size: "sm",
+      },
+    },
+    Button: {
+      variants: {
+        outline: {
+          bg: "white",
+        },
+      },
+      defaultProps: {
+        variant: "solid",
+        size: "md",
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
