40561bf443170183991d750c00e8b8284d914f8b
hilmanauz <hilmanauz@gmail.com>
2022-02-16 16:16:29 +0700

    update theme.tsx

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider, theme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import Head from 'next/head'
+import theme from './theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
