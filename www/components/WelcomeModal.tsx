a17ff24a9dd9cb8a4a8e44db8506ab5883644df4
hilmanauz <hilmanauz@gmail.com>
2022-02-25 19:03:28 +0700

    handle error ' in welcomeModal

--- www/components/WelcomeModal.tsx
+++ www/components/WelcomeModal.tsx
@@ -9,7 +9,9 @@ function WelcomeModal({ disclosure }: { disclosure: UseDisclosureProps }) {
       <Box width={"full"} height={"10%"} className={styles.border} borderTopRadius={"10px"} />
       <VStack textAlign={"center"} height={"90%"} width={"full"} paddingY={"10px"} paddingX={"32px"} spacing={1} color={"black"}>
         <Text className={styles.welcomeHeader} fontSize={["40px", "60px"]}>Hello..</Text>
-        <Text paddingX={"50px"} fontWeight={"600"} fontSize={["13px", "20px"]} className={styles.welcomeBody}>Welcome at New Land Entertainment. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took galley of type and scrambled it to make a type specimen book</Text>
+        <Text paddingX={"50px"} fontWeight={"600"} fontSize={["13px", "20px"]} className={styles.welcomeBody}>{
+          "Welcome at New Land Entertainment. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took galley of type and scrambled it to make a type specimen book"
+        }</Text>
         <Spacer />
         <HStack justifyContent={"flex-end"} width={"full"}>
           <Button fontFamily={"MontBold"} colorScheme={"gray"} variant={"ghost"} onClick={disclosure.onOpen} _focus={{ borderWidth: "0px" }}>Next <Icon as={BiArrowToRight} /></Button> </HStack>
